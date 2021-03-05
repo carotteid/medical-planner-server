41bfa11aae3b009bbaadd2c4191aa7497566986d
Id Avila <i.avilacamargo@ugto.mx>
2021-03-05 17:40:01 -0600

    add deleted app.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 5030;
+const PORT = process.env.PORT || 3000;
 //app.listen(PORT);
 
 app.use("/api", routes);
