12a904ecc11cf248448a79c057e3c6f473ad7e29
Id Avila <i.avilacamargo@ugto.mx>
2021-03-05 16:42:45 -0600

    add deleted app.js

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const http = require('http');
 
 const app = express();
+
+const port = process.env.PORT || 5015
 
 app.use(cors());
 
@@ -16,6 +19,16 @@ app.use(
   })
 );
 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
+
 app.use("/api", routes);
 
-app.listen(5009, () => console.log("Server is up"));
+//app.listen(port, () => console.log("Server is up"));
